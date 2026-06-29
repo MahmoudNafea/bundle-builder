9f07f61eeeb3f9e42a2b34653c7469724fa55467
mahmoud <mahmouddeveloper19@gmail.com>
2026-06-29 11:37:24 +0300

    feat: mapping css to tokens file and reuse it

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,15 @@
-import { AccordionStep } from "./components/builder/AccordionStep";
 import { BuilderPanel } from "./components/builder/BuilderPanel";
-import { ProductCard } from "./components/builder/ProductCard";
 import { ReviewPanel } from "./components/review/ReviewPanel";
 import { BundleProvider } from "./context/BundleContext";
-import productsData from './data/products.json';
+import productsData from "./data/products.json";
 import { Step } from "./types";
-import './App.css';
+import "./App.css";
 
 function App() {
-    const firstProduct = productsData.steps[0].products[0];
-    const steps = productsData.steps as Step[];
-
+  const steps = productsData.steps as Step[];
 
   return (
-    //   <BundleProvider>
-    //   <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-    //     <h1>Bundle Builder</h1>
-    //     <p>App is working </p>
-    //   </div>
-    // </BundleProvider>
-    //   <BundleProvider>
-    //   <div style={{ padding: '2rem', maxWidth: '400px' }}>
-    //     <ProductCard product={firstProduct} />
-    //   </div>
-    // </BundleProvider>
-    //     <BundleProvider>
-    //   <div style={{ maxWidth: '800px', margin: '2rem auto', padding: '0 1rem', display: 'flex', flexDirection: 'column', gap: '12px' }}>
-    //     {steps.map((step, index) => (
-    //       <AccordionStep key={step.id} step={step} stepIndex={index} />
-    //     ))}
-    //   </div>
-    // </BundleProvider>
-
-       <BundleProvider>
+    <BundleProvider>
       <div className="app-layout">
         <BuilderPanel steps={steps} />
         <ReviewPanel steps={steps} />
